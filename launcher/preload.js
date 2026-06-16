e2e1accb686b4a101152221532a42c974d4c1043
npscott202-stack <npscott202@gmail.com>
2026-06-16 01:13:20 -0400

    Add Electron preload script

---
+++ launcher/preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  platform: process.platform,
+  versions: process.versions,
+  send: (channel, data) => {
+    ipcRenderer.send(channel, data);
+  },
+  receive: (channel, func) => {
+    ipcRenderer.on(channel, (event, ...args) => func(...args));
+  }
+});
